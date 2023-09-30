f7a06da10761a71724d5fa0b780fc22433aba421
SpaceyLake <niklas_hornblad@hotmail.com>
2023-09-30 19:29:35 +0200

    Continued on enemies

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -1,2 +1,9 @@
 extends Node2D
 class_name Enemy
+
+var goal:Vector2
+var enemy_killed_signal:Signal
+
+func setup(goal:Vector2, enemy_killed_signal:Signal):
+	self.goal = goal
+	self.enemy_killed_signal = enemy_killed_signal
--- scripts/enemies/fighter.gd
+++ scripts/enemies/fighter.gd
@@ -1,1 +1,6 @@
-extends "res://scripts/enemies/enemy.gd"
+extends Enemy
+
+@export var velocity = 5
+
+func _process(delta):
+	global_position = global_position.move_toward(goal, velocity)
