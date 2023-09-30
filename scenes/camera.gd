4b03c03f2dfd5957bd448bf4e781540b1c253deb
Golahirom <golahirom@gmail.com>
2023-09-30 21:36:00 +0200

    Implemented camera

---
+++ scenes/camera.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@export var shake_random_strength: float = 30.0
+@export var shake_decay_rate: float = 5.0
+@export var shake_noise_speed: float = 30.0
+@export var shake_noise_strength: float = 60.0
+
+@onready var rand = RandomNumberGenerator.new()
+@onready var noise = FastNoiseLite.new()
+@onready var camera = $Camera2D
+
+var noise_i: float = 0.0
+var shake_strength: float = 0.0
+
+func _ready():
+	noise.seed = rand.randi()
+	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	noise.frequency = 0.5
+
+func apply_shake(strength: float = shake_noise_strength) -> void:
+	shake_strength = strength
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		apply_shake()
+
+func _process(delta: float) -> void:
+	shake_strength = lerp(shake_strength, 0.0, shake_decay_rate * delta)
+	camera.offset = get_noise_offset(delta)
+
+func get_noise_offset(delta: float) -> Vector2:
+	noise_i += delta * shake_noise_speed
+	return Vector2(
+		noise.get_noise_2d(1, noise_i) * shake_strength,
+		noise.get_noise_2d(100, noise_i) * shake_strength
+	)
