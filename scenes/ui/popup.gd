c1694668a6353f3a62db5e908a6580c2e8d86bb2
Savalige <matsbennervall@gmail.com>
2023-10-01 19:04:23 +0200

    Popup

--- scenes/ui/popup.gd
+++ scenes/ui/popup.gd
@@ -1,1 +1,20 @@
 extends Node2D
+
+@export var text:Array = []
+@export var timer:Timer
+@export var text_holder:TextEdit
+@export var button:Button
+
+func _ready():
+	timer.timeout.connect(Callable(self,"show_popup"))
+	timer.start(5)
+
+func show_popup():
+	if not text.is_empty():
+		if not visible:
+			visible = true
+		
+		text_holder.text = text[0]
+		text.remove_at(0)
+	else:
+		visible = false
