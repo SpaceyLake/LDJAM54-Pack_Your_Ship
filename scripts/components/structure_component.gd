cbaf385219880a67e6d7ead4673944af5aed3970
Savalige <matsbennervall@gmail.com>
2023-10-01 10:59:03 +0200

    Working on neighbors

--- scripts/components/structure_component.gd
+++ scripts/components/structure_component.gd
@@ -12,6 +12,10 @@ class_name StructureComponent
 @onready var spaceship:SpaceShip = get_parent() if get_parent() is SpaceShip else null
 
 func _ready():
+	for x in spaceship.components.size():
+		for y in spaceship.components[x].size():
+			if spaceship.components[x][y] == self:
+				pos = Vector2(x,y)
 	health.health_depleted.connect(Callable(self, "on_death"))
 
 func place(new_position:Vector2):
